19c22d7cf620acfa7ba1b0cc64c2de8febf534ff
Arjun Chib <arjunpatrickchib@gmail.com>
2020-08-20 21:28:42 -0400

    Add bot and donation links

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1>Hello world!</h1>
+  <div>
+    <h1>memebot</h1>
+    <a
+      href="https://discord.com/oauth2/authorize?client_id= 350733228098715658&scope=bot&permissions=3147840"
+      >Add to Discord</a
+    >
+    <a href="https://ko-fi.com/memebot">Buy me a Coffee!</a>
+  </div>
 </template>
